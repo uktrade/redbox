182db4fa4e5f8a72bbb7325640f62571120ff620
Natasha Boyse <natasha.boyse@digital.trade.gov.uk>
2025-01-20 14:44:08 +0000

    feat: api middleware integration and fetch all user history

--- django_app/redbox_app/redbox_core/views/api_views.py
+++ django_app/redbox_app/redbox_core/views/api_views.py
@@ -1,18 +1,17 @@
 from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from redbox_app.redbox_core.serializers import UserSerializer
 
 User = get_user_model()
 
-
 @api_view(["GET"])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated, IsAdminUser])
 def user_view_pre_alpha(request):
     """this is for testing and evaluation only
     this *will* change so that not all data is returned!
     """
-    serializer = UserSerializer(request.user)
+    serializer = UserSerializer(User.objects.all(), many=True, read_only=True)
     return Response(serializer.data)
