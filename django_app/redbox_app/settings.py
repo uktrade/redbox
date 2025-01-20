9229807570dabf1bd76b5ad7121adbc815b203cc
larry6p6 <larry.adeyemi@6point6.co.uk>
2025-01-20 10:58:11 +0000

    remove redundant GTM SHAs

--- django_app/redbox_app/settings.py
+++ django_app/redbox_app/settings.py
@@ -187,10 +187,8 @@ CSP_DEFAULT_SRC = (
 
 CSP_SCRIPT_SRC = (
     "'self'",
-    "'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='",
     "eu.i.posthog.com",
     "eu-assets.i.posthog.com",
-    "'sha256-qmCu1kQifDfCnUd+L49nusp7+PeRl23639pzN5QF2WA='",
     "'sha256-RfLASrooywwZYqv6kr3TCnrZzfl6ZTfbpLBJOVR/Gt4='"
     "https://tagmanager.google.com/",
     "https://www.googletagmanager.com/",
