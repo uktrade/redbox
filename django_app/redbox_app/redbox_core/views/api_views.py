cad9d8eb4dd5d9e812bf12df0c3a30f79fe47c53
agalamatis <argyris.galamatis@digital.trade.gov.uk>
2025-02-11 11:33:29 +0000

    feat: new messages API endpoint
    The existing users endpoint, used by data-flow in order to ingest the data into
    Data Workspace, can be extremely slow in production, to the point that it now
    always hits the CloudFront timeout at 30 seconds. The endpoint is paginated,
    and it does get a few pages in, but because a each User entry includes all of
    their chats, which also includes all of their chat messages, it suspect that
    this causes the endpoint to be too slow when it hits a user with a lot of
    chats/messages.
    
    In more detail, it's suspected that the slowness comes from 2 things:
    
    1. The number database queries made to construct the data (no prefect_related
       or select_releated or similar is used)
    2. And the amount of data - and so
      transfer time from the database and time it takes to convert it all to JSON
    
    This add a new messages endpoint that right now _just_ returns the chat
    messages themselves. This should hopefully not be subject to either 2. or 3.
    above. It shouldn't hit 2 because each page _should_ be one query, and
    shouldn't hit 3 because there is an effective maximum to the length of each
    message, and there are a fixed number of messages per page.
    
    This is not all the data that is needed in Data Workspace - also needed are the
    Chats and Users. There are essentially two options for this: an endpoint per
    model, or adding in the users and chats right into each message. However, these
    are left until a later change to verify that this approach does work, and
    ultimately the messages are the core any analysis in Data Workspace, and so
    also we judge to be the most important.
    
    A future change could also change the pagination mechanism to cursor
    pagination, which should be more efficient that page (which is limit/offset
    under the hood). However, right now it's not suspected that this is an issue in
    the slowness, so is not done here.
    
    Co-authored-by: Michal Charemza <michal@charemza.name>
    Co-authored-by: Sekhar Panja <sekhar.panja@digital.trade.gov.uk>
    Co-authored-by: Ares Galamatis <argyris.galamatis@digital.trade.gov.uk>

--- django_app/redbox_app/redbox_core/views/api_views.py
+++ django_app/redbox_app/redbox_core/views/api_views.py
@@ -3,7 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-from redbox_app.redbox_core.serializers import UserSerializer
+from redbox_app.redbox_core.models import ChatMessage
+from redbox_app.redbox_core.serializers import ChatMessageSerializer, UserSerializer
 
 User = get_user_model()
 
@@ -22,4 +23,15 @@ def user_view_pre_alpha(request):
     queryset = User.objects.all()
     result_page = paginator.paginate_queryset(queryset, request)
     serializer = UserSerializer(result_page, many=True, read_only=True)
+    return paginator.get_paginated_response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated, IsAdminUser])
+def message_view_pre_alpha(request):
+    """Return paginated message data"""
+    paginator = StandardResultsSetPagination()
+    queryset = ChatMessage.objects.all()
+    result_page = paginator.paginate_queryset(queryset, request)
+    serializer = ChatMessageSerializer(result_page, many=True, read_only=True)
     return paginator.get_paginated_response(serializer.data)
