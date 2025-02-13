917f81db6dca62f1898f879ddc0492b3b2a7cea7
Natasha Boyse <natasha.boyse@digital.trade.gov.uk>
2025-02-13 15:24:52 +0000

    rebase

--- django_app/redbox_app/redbox_core/migrations/0078_alter_user_email_alter_user_username.py
+++ django_app/redbox_app/redbox_core/migrations/0078_alter_user_email_alter_user_username.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("redbox_core", "0077_alter_user_username"),
+        ("redbox_core", "0077_aisettings_new_route_retrieval_system_prompt"),
     ]
 
     operations = [
