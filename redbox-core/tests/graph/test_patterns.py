9a45cf5c8355d8ebd1a448cceaea560a666e421a
nora-errouhly <nora.er-rouhly@digital.trade.gov.uk>
2025-04-02 16:42:36 +0100

    ruff again

--- redbox-core/tests/graph/test_patterns.py
+++ redbox-core/tests/graph/test_patterns.py
@@ -491,9 +491,7 @@ LLM_ROUTE_TEST_CASE = generate_test_cases(
     ids=[t.test_id for t in LLM_ROUTE_TEST_CASE],
 )
 def test_llm_choose_route(test_case: RedboxChatTestCase, basic_metadata: BaseRetriever, mocker: MockerFixture):
-    mocker.patch(
-        "redbox.chains.runnables.get_basic_metadata_retriever", return_value=basic_metadata(test_case.docs)
-    )
+    mocker.patch("redbox.chains.runnables.get_basic_metadata_retriever", return_value=basic_metadata(test_case.docs))
     llm = GenericFakeChatModel(messages=iter(test_case.test_data.llm_responses))
     llm._default_config = {"model": "bedrock"}
 
