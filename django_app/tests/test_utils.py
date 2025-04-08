e1ab61cd9956a2aa64cabb34aa5440945763e59a
Natasha Boyse <natasha.boyse@digital.trade.gov.uk>
2025-04-08 14:18:20 +0100

    fix: tests

--- django_app/tests/test_utils.py
+++ django_app/tests/test_utils.py
@@ -10,12 +10,12 @@ from redbox_app.redbox_core.utils import get_date_group
     ("given", "expected"),
     [
         (timezone.now().date(), "Today"),
-        ((timezone.now() - timedelta(days=1)).date(), "Yesterday"),
-        ((timezone.now() - timedelta(days=2)).date(), "Previous 7 days"),
-        ((timezone.now() - timedelta(days=7)).date(), "Previous 7 days"),
-        ((timezone.now() - timedelta(days=8)).date(), "Previous 30 days"),
-        ((timezone.now() - timedelta(days=30)).date(), "Previous 30 days"),
-        ((timezone.now() - timedelta(days=31)).date(), "Older than 30 days"),
+        ((timezone.now() - timedelta(days=1)).date(), "Previous"),
+        ((timezone.now() - timedelta(days=2)).date(), "Previous"),
+        ((timezone.now() - timedelta(days=7)).date(), "Previous"),
+        ((timezone.now() - timedelta(days=8)).date(), "Previous"),
+        ((timezone.now() - timedelta(days=30)).date(), "Previous"),
+        ((timezone.now() - timedelta(days=31)).date(), "Previous"),
     ],
 )
 def test_date_group_calculation(given: date, expected: str):
