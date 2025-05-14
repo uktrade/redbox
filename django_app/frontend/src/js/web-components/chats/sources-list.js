d27463d8f4d1334ca2ffe2d9bb49b24a1a29156a
Natasha Boyse <natasha.boyse@digital.trade.gov.uk>
2025-05-14 16:43:14 +0100

    feat: go to source instead of file download

--- django_app/frontend/src/js/web-components/chats/sources-list.js
+++ django_app/frontend/src/js/web-components/chats/sources-list.js
@@ -5,6 +5,7 @@ export class SourcesList extends HTMLElement {
     super();
     this.sources = [];
   }
+
   // citations text sometimes contains quotation marks which breaks the link formed by data-text
   // The Function below helps preserve this information by converting it from an HTML attribute thus avoiding any issues.
   escapeHtmlAttribute(str) {
@@ -19,44 +20,51 @@ export class SourcesList extends HTMLElement {
       return str; // Fallback to original string
     }
   }
+
   /**
    * Adds a source to the current message
    * @param {string} fileName
    * @param {string} url
    * @param {string} matchingText
+   * @param {string} chatId
    */
-  add = (fileName, url, matchingText) => {
+  add = (fileName, url, matchingText, chatId) => {
 
     // prevent duplicate sources
     if (this.sources.some((source) => source.matchingText === matchingText)) {
       return;
     }
 
+    // generate a unique footnote ID
+    const footnoteId = crypto.randomUUID();
+
     this.sources.push({
       fileName: fileName,
       url: url,
       matchingText: matchingText,
+      footnoteId: footnoteId,
     });
 
     let html = `
-            <h3 class="iai-chat-bubble__sources-heading govuk-heading-s govuk-!-margin-bottom-1">Sources</h3>
-            <div class="iai-display-flex-from-desktop">
-            <ol class="rb-footnote-list govuk-!-margin-bottom-0">
-        `;
-    this.sources.forEach((source) => {
+      <h3 class="iai-chat-bubble__sources-heading govuk-heading-s govuk-!-margin-bottom-1">Sources</h3>
+      <div class="iai-display-flex-from-desktop">
+      <ol class="rb-footnote-list govuk-!-margin-bottom-0">
+    `;
+    this.sources.forEach((source, index) => {
       html += `
-                <li class="govuk-!-margin-bottom-0">
-                    <a class="iai-chat-bubbles__sources-link govuk-link" href="${
-                      source.url
-                    }" id="footnote-${this.getAttribute("data-id")}-${
-        this.sources.length
-      }" data-text="${this.escapeHtmlAttribute(source.matchingText)}">${source.fileName || source.url}</a>
-                </li>
-            `;
+        <li class="govuk-!-margin-bottom-0">
+          <a class="iai-chat-bubbles__sources-link govuk-link" 
+             href="/citations/${chatId}/#${source.footnoteId}" 
+             id="footnote-${source.footnoteId}" 
+             data-text="${this.escapeHtmlAttribute(source.matchingText)}">
+            ${source.fileName || source.url}
+          </a>
+        </li>
+      `;
     });
     html += `</div></ol>`;
     this.innerHTML = html;
   };
+}
 
-  }
 customElements.define("sources-list", SourcesList);
