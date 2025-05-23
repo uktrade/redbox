3e2a2ef84165c0774d8492756f9a38edcf9e49a4
Ugo Okoroafor <ugo.okoroafor@digital.trade.gov.uk>
2025-05-23 08:40:44 +0100

    Fixing issue in pre-build script (#263)

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -7,4 +7,4 @@ cp -r "redbox-core" "django_app"
 cp "Procfile" "django_app/Procfile"
 
 cd django_app
-sed -i 's|../redbox-core|redbox-core' pyproject.toml
+sed -i 's|../redbox-core|redbox-core|' pyproject.toml
