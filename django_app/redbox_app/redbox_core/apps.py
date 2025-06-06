53aed967f1b6c6b0e6d66030e0a55accba42e01b
Natasha Boyse <natasha.boyse@digital.trade.gov.uk>
2025-06-06 15:58:35 +0100

    Revert "create last login too"
    This reverts commit b78c56849fa6b756593789526bc9800d3cc8bef1.

--- django_app/redbox_app/redbox_core/apps.py
+++ django_app/redbox_app/redbox_core/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class RedboxCoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "redbox_app.redbox_core"
-
-    def ready(self):
-        pass
