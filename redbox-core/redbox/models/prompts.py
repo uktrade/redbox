866cbd91d520833173035b9914e7cdb83aef68e3
Ugo Okoroafor <ugo.okoroafor@digital.trade.gov.uk>
2025-06-06 11:56:32 +0100

    Strengthening citations prompt

--- redbox-core/redbox/models/prompts.py
+++ redbox-core/redbox/models/prompts.py
@@ -16,7 +16,7 @@ CHAT_WITH_DOCS_SYSTEM_PROMPT = "You are tasked with providing information object
 CITATION_PROMPT = """Use citations to back up your answer when available. Return your response in the following format: {format_instructions}.
 Example response:
 If citations are available: {{"answer": "your complete answer here including any 'ref_N' citation markers inline as required in plain text", "citations": [list_of_citations]}}.
-- Each citation must be shown in the answer using a unique identifier in the format "ref_N" where N is an incrementing number starting from 1.
+- Each citation must be shown in the answer using a unique identifier in the format "ref_N" where N is an incrementing number starting from 1. DO NOT REPEAT CITATIONS OR CITATION IDENTIFIERS.
 
 If no citations are available or needed, return an empty array for citations like this: {{"answer": "your complete answer here with no citation markers", "citations": []}}.
 Do not provide citation from your own knowledge.
