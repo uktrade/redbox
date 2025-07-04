9a155921d9c5b61276ffba35b7bc299596218037
Saisakul Chernbumroong <159020482+saisakul@users.noreply.github.com>
2025-07-04 13:09:06 +0100

    add missing lib for data dog (#365)
    * add missing lib for data dog
    
    * fix ddtrace call
    
    ---------
    
    Co-authored-by: Saisakul Chernbumroong <saisakulchernbumroong@DBT000687.local>

--- django_app/start.sh
+++ django_app/start.sh
@@ -8,4 +8,4 @@ venv/bin/django-admin create_admin_user
 
 echo "Starting daphne on port $PORT"
 #venv/bin/daphne --websocket_timeout 86400 -b 0.0.0.0 -p $PORT redbox_app.asgi:application
-ddtrace-run venv/bin/daphne --websocket_timeout 86400 -b 0.0.0.0 -p $PORT redbox_app.asgi:application
+venv/bin/ddtrace-run venv/bin/daphne --websocket_timeout 86400 -b 0.0.0.0 -p $PORT redbox_app.asgi:application
