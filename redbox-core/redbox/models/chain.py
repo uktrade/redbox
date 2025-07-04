32f9cc6282837f034a755fbe3c0b101e0c8ef5d1
Richie Bird <richie.bird@trade.gov.uk>
2025-07-04 16:50:32 +0100

    combined StructuredResponseWithCitations and StructuredResponseWithCitationsProcess

--- redbox-core/redbox/models/chain.py
+++ redbox-core/redbox/models/chain.py
@@ -155,11 +155,6 @@ class Citation(BaseModel):
 class StructuredResponseWithCitations(BaseModel):
     answer: str = Field(description="Markdown structured answer to the question", default="")
     citations: list[Citation] = Field(default_factory=list)
-
-
-class StructuredResponseWithCitationsProcess(BaseModel):
-    answer: str = Field(description="Markdown structured answer to the question", default="")
-    citations: list[Citation] = Field(default_factory=list)
     answer_process: str = Field(
         description="Outline using bullet points, the processes using you took to produce the answer within 200 words or less and highlight any process that were unsuccessful in answering the user question",
         default="",
