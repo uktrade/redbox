2da8ea0f09d00c7378787dfdbb9b09bb9f03238a
David Okeke <140066072+david-okeke1337@users.noreply.github.com>
2025-10-20 14:04:29 +0100

    Feature/teams admin (#507)
    * Added team admin settings
    
    * Removed invalid migration
    
    * Attempt to fix invalid model ref
    
    * added package ref
    
    * reverted package change
    
    * Delete redbox/__init__.py
    
    ---------
    
    Co-authored-by: Tash Boyse <57753415+nboyse@users.noreply.github.com>

--- django_app/redbox_app/redbox_core/admin.py
+++ django_app/redbox_app/redbox_core/admin.py
@@ -176,6 +176,27 @@ class UserAdmin(ImportExportMixin, admin.ModelAdmin):
         import_id_fields = ["email"]
 
 
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("team_name", "directorate", "created_at", "updated_at")
+    list_filter = ("directorate",)
+    search_fields = ("team_name", "directorate")
+    ordering = ("team_name",)
+
+    class Meta:
+        model = models.Team
+
+
+class UserTeamMembershipAdmin(admin.ModelAdmin):
+    list_display = ("user", "team", "role_type", "created_at", "updated_at")
+    list_filter = ("role_type", "team__directorate")
+    search_fields = ("user__email", "team__team_name")
+    ordering = ("user__email",)
+    raw_id_fields = ("user", "team")
+
+    class Meta:
+        model = models.UserTeamMembership
+
+
 class FileAdmin(ExportMixin, admin.ModelAdmin):
     def reupload(self, _request, queryset):
         for file in queryset:
@@ -353,4 +374,6 @@ admin.site.register(models.ChatLLMBackend, ChatLLMBackendAdmin)
 admin.site.register(models.MonitorSearchRoute, MonitorSearchRouteAdmin)
 admin.site.register(models.MonitorWebSearchResults, MonitorWebSearchResultsAdmin)
 admin.site.register(models.AgentPlan, AgentPlanAdmin)
+admin.site.register(models.Team, TeamAdmin)
+admin.site.register(models.UserTeamMembership, UserTeamMembershipAdmin)
 admin.site.register_view("report/", view=reporting_dashboard, name="Site report")
