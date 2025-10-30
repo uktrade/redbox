7ba7dc6b2e751e6928da1a30d47ec50d5fd1344f
Natasha Boyse <natasha.boyse@digital.trade.gov.uk>
2025-10-30 13:57:23 +0000

    add int field for max tokens

--- django_app/redbox_app/redbox_core/models.py
+++ django_app/redbox_app/redbox_core/models.py
@@ -1130,6 +1130,7 @@ class AgentPlan(UUIDPrimaryKeyBase, TimeStampedModel):
 class Agent(UUIDPrimaryKeyBase, TimeStampedModel):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
+    agents_max_tokens = models.PositiveIntegerField(blank=True, null=True)
 
     llm_backend = models.ForeignKey(
         ChatLLMBackend,
