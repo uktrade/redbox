f5b1fdc6362fcb64ade2023436fe2da248a115bd
Doug Mills <110824173+dougmills-DIT@users.noreply.github.com>
2025-11-04 15:40:28 +0000

    Add admin for Agent, Skill and AgentSkill (#547)
    * Add admin for Agent, Skill and AgentSkill
    
    * Update the display style of agens list view, removing prompt field
    Update the edit / create form to reduce the height of the description field and increate the height of the prompt field
    
    * code formatting
    
    * code formatting
    
    * code formatting

--- django_app/redbox_app/redbox_core/admin.py
+++ django_app/redbox_app/redbox_core/admin.py
@@ -30,6 +30,51 @@ class ChatLLMBackendAdmin(admin.ModelAdmin):
         model = models.ChatLLMBackend
 
 
+class SkillAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "description",
+    ]
+
+    search_fields = ["name"]
+    readonly_fields = ["modified_at", "created_at"]
+
+    class Meta:
+        model = models.Skill
+
+
+class AgentSkillAdmin(admin.ModelAdmin):
+    list_display = [
+        "agent__name",
+        "skill__name",
+    ]
+
+    search_fields = ["agent__name", "skill__name"]
+    readonly_fields = ["modified_at", "created_at"]
+
+    class Meta:
+        model = models.AgentSkill
+
+
+class AgentAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "description",
+    ]
+
+    search_fields = ["name"]
+    readonly_fields = ["modified_at", "created_at"]
+
+    class Meta:
+        model = models.Agent
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["prompt"].widget.attrs["rows"] = "40"
+        form.base_fields["description"].widget.attrs["rows"] = "5"
+        return form
+
+
 class UserAdmin(ImportExportMixin, admin.ModelAdmin):
     def export_as_json(self, request, queryset: QuerySet):  # noqa:ARG002
         user_data = UserSerializer(many=True).to_representation(queryset)
@@ -396,4 +441,7 @@ admin.site.register(models.AgentPlan, AgentPlanAdmin)
 admin.site.register(models.Team, TeamAdmin)
 admin.site.register(models.UserTeamMembership, UserTeamMembershipAdmin)
 admin.site.register(models.FileTeamMembership, FileTeamMembershipAdmin)
+admin.site.register(models.Agent, AgentAdmin)
+admin.site.register(models.Skill, SkillAdmin)
+admin.site.register(models.AgentSkill, AgentSkillAdmin)
 admin.site.register_view("report/", view=reporting_dashboard, name="Site report")
