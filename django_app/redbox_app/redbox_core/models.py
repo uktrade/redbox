1c76caaae80f417f3c963ee539d2433d8db4bc01
David Okeke <david.okeke@madetech.com>
2025-11-18 10:58:31 +0000

    Updated citation ref tests and added help text for skill slug

--- django_app/redbox_app/redbox_core/models.py
+++ django_app/redbox_app/redbox_core/models.py
@@ -86,7 +86,9 @@ class Skill(UUIDPrimaryKeyBase, TimeStampedModel):
 
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
-    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    slug = models.SlugField(
+        max_length=100, unique=True, blank=True, help_text="Used for url routing and info page linking"
+    )
 
     class Meta:
         verbose_name_plural = "skills"
