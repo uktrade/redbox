474e9a3dccfedc9d536cbf2ad51ed3942c9b6705
Saisakul Chernbumroong <saisakulchernbumroong@DBT000687.local>
2025-11-20 12:50:46 +0000

    add tests

--- redbox/tests/graph/nodes/test_sends.py
+++ redbox/tests/graph/nodes/test_sends.py
@@ -1,11 +1,19 @@
 from uuid import uuid4
 
 from langchain_core.documents import Document
-from langchain_core.messages import ToolCall, AIMessage
+from langchain_core.messages import AIMessage, ToolCall
 from langgraph.constants import Send
+from pytest_mock import MockerFixture
 
-from redbox.graph.nodes.sends import build_document_chunk_send, build_document_group_send, build_tool_send
+from redbox.graph.nodes.sends import (
+    build_document_chunk_send,
+    build_document_group_send,
+    build_tool_send,
+    run_tools_parallel,
+)
+from redbox.graph.nodes.tools import build_search_wikipedia_tool
 from redbox.models.chain import DocumentState, RedboxQuery, RedboxState
+from tests.conftest import fake_state
 
 
 def test_build_document_group_send():
@@ -106,3 +114,45 @@ def test_build_tool_send():
         ),
     ]
     assert expected == actual
+
+
+class TestRunToolsParallel:
+    def test_no_tool_call(self, fake_state):
+        ai_msg = AIMessage(content="no tool call", tool_calls=[])
+
+        response = run_tools_parallel(ai_msg=ai_msg, tools=[], state=fake_state)
+
+        assert isinstance(response, str)
+        assert response == "no tool call"
+
+    def test_no_tool_found(self, fake_state):
+        search_wikipedia = build_search_wikipedia_tool()
+        ai_msg = AIMessage(
+            content="I am calling a tool",
+            tool_calls=[{"name": "_test_tool", "args": {"query": "fake query"}, "id": "1"}],
+        )
+
+        response = run_tools_parallel(ai_msg=ai_msg, tools=[search_wikipedia], state=fake_state)
+
+        assert response == []
+
+    def test_tool_found(self, mocker: MockerFixture):
+        # mock tool
+        mock_tool = mocker.patch("langchain_community.utilities.WikipediaAPIWrapper.load")
+        mock_tool.return_value = [
+            Document(
+                metadata={"title": "fake title", "summary": "fake summary", "source": "https://fake.com"},
+                page_content="Fake content",
+            )
+        ]
+
+        search_wikipedia = build_search_wikipedia_tool()
+        ai_msg = AIMessage(
+            content="I am calling a tool",
+            tool_calls=[{"name": "_search_wikipedia", "args": {"query": "fake query"}, "id": "1"}],
+        )
+
+        response = run_tools_parallel(ai_msg=ai_msg, tools=[search_wikipedia], state=fake_state)
+
+        assert isinstance(response, list)
+        assert len(response[0].content) > 0
