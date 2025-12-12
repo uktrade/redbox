927316b7b311fb583177d5bacfdb4641c70f5d0f
Saisakul Chernbumroong <saisakulchernbumroong@DBT000687.local>
2025-12-12 11:04:49 +0000

    add agent config

---
+++ redbox/redbox/agents/agents.py
@@ -0,0 +1,98 @@
+import logging
+from abc import ABC, abstractmethod
+
+from langchain_core.runnables import RunnableLambda, RunnableParallel
+
+from redbox.agents.configs import AgentConfig
+from redbox.chains.parser import ClaudeParser
+from redbox.graph.nodes.processes import build_activity_log_node
+from redbox.models.chain import RedboxState, configure_agent_task_plan
+from redbox.models.graph import RedboxActivityEvent
+from redbox.transform import bedrock_tokeniser, truncate_to_tokens
+
+log = logging.getLogger(__name__)
+
+
+class Agent(ABC):
+    def __init__(self, config):
+        self.config: AgentConfig = config
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+
+class WorkerAgent(Agent):
+    def __init__(self, config: AgentConfig):
+        super().__init__(config)
+        self.task = None
+
+    def reading_task_info(self, state: RedboxState):
+        # dynamically generate agent task based on state
+        agent_options = {agent.name: agent.name for agent in state.request.ai_settings.worker_agents}
+        ConfiguredAgentTask, _ = configure_agent_task_plan(agent_options)
+        parser = ClaudeParser(pydantic_object=ConfiguredAgentTask)
+        try:
+            self.task = parser.parse(state.last_message.content)
+        except Exception as e:
+            log.exception(f"Cannot parse task in {self.config.name}: {e}")
+
+    def log_agent_activity(
+        self,
+        state: RedboxState,
+    ):
+        activity_node = build_activity_log_node(
+            RedboxActivityEvent(message=f"{self.config.name} is completing task: {self.task.task}")
+        )
+        activity_node.invoke(state)
+
+    def post_processing(self, result):
+        if isinstance(result, str):
+            log.warning(f"[{self.config.name}] Using raw string result.")
+            result_content = result
+        elif isinstance(result, list) and isinstance(result[0], dict):
+            log.warning(f"[{self.config.name}] Using raw string in a list as result.")
+            result_content = result[0].get("text", "")
+        elif isinstance(result, list):
+            log.warning(f"[{self.config.name}] Aggregating list of tool results...")
+            result_content = []
+            current_token_counts = 0
+
+            for res in result:
+                token_count = bedrock_tokeniser(res.content)
+                log.warning(f"[{self.config.name}] Tool response token count: {token_count}")
+
+                # If adding this whole piece still fits, append normally
+                if current_token_counts + token_count <= self.config.max_tokens:
+                    result_content.append(res.content)
+                    current_token_counts += token_count
+                else:
+                    # If no room, add only what fits
+                    remaining_tokens = self.config.max_tokens - current_token_counts
+                    if remaining_tokens > 0:
+                        log.warning(
+                            f"[{self.config.name}] Truncating tool output to fit remaining token budget ({remaining_tokens})."
+                        )
+                        truncated = truncate_to_tokens(res.content, remaining_tokens)
+                        result_content.append(truncated)
+                        current_token_counts += bedrock_tokeniser(truncated)
+                    else:
+                        log.warning(
+                            f"[{self.config.name}] No remaining token budget ({self.config.max_tokens}). Skipping."
+                        )
+                    break  # Max reached — stop processing further results
+            result_content = " ".join(result_content)
+        else:
+            log.error(f"[{self.config.name}] Worker agent return incompatible data type {type(result)}")
+            raise TypeError("Invalid tool result type")
+        log.warning(f"[{self.config.name}] Completed agent run.")
+        return {
+            "agents_results": f"<{self.config.name}_Result>{result_content}</{self.config.name}_Result>",
+            "tasks_evaluator": self.task.task + "\n" + self.task.expected_output,
+        }
+
+    @RunnableLambda
+    def execute(self, state: RedboxState):
+        return self.reading_task_info | RunnableParallel(
+            log=self.log_agent_activity, result=self.core_task.with_retry(stop_after_attempt=3) | self.post_processing
+        )
