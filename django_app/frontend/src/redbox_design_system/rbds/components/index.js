a2e6060b16d07773e432cfcfeac189cb918570fa
David Okeke <140066072+david-okeke1337@users.noreply.github.com>
2026-01-05 16:28:13 +0000

    Moved activity message to loading-message component (#693)

--- django_app/frontend/src/redbox_design_system/rbds/components/index.js
+++ django_app/frontend/src/redbox_design_system/rbds/components/index.js
@@ -1,6 +1,8 @@
 export * from './accessible-autocomplete.js';
 export * from './editable-text.js';
+export * from './loading-message.js';
 export * from './modal.js';
 export * from './show-more.js';
+// export * from './side-panel.js';
 export * from './uploaded-file.js';
 export * from './uploaded-files.js';
