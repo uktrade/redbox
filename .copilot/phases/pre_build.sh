849cb926a8789b9657d45e887c666b47c73b11ff
Natasha Boyse <natasha.boyse@digital.trade.gov.uk>
2026-01-16 09:46:18 +0000

    fix: codepipeline integration with Procfile

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -4,6 +4,7 @@ set -e
 cp -r ".git" "django_app"
 cp -r ".copilot" "django_app"
 cp -r "redbox" "django_app"
+cp -r "django_app/start-aws.sh" "django_app/start-aws.sh"
 cp "Procfile" "django_app/Procfile"
 
 cd django_app
