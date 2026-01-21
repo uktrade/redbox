cca344b007c64bb2ea4f7a864ea7e80b729ba222
Tash Boyse <57753415+nboyse@users.noreply.github.com>
2026-01-21 15:30:32 +0000

    chore: use env vars not hardcoded domains (#752)

--- django_app/redbox_app/setting_enums.py
+++ django_app/redbox_app/setting_enums.py
@@ -27,10 +27,10 @@ class Environment(StrEnum):
 
     LOCAL = ("LOCAL", True, [*LOCAL_HOSTS, *ADDITIONAL_HOSTS])  # nosec: B104: Not in prod
     INTEGRATION = ("INTEGRATION", True, [*LOCAL_HOSTS, *ADDITIONAL_HOSTS])  # nosec: B104: Not in prod
-    DEV = ("DEV", False, ["dev.redbox.uktrade.digital", "dev.assist.uktrade.digital", *ADDITIONAL_HOSTS])
-    UAT = ("UAT", False, ["uat.redbox.uktrade.digital", "uat.assist.uktrade.digital", *ADDITIONAL_HOSTS])
-    LAB = ("LAB", False, ["lab.redbox.uktrade.digital", *ADDITIONAL_HOSTS])
-    PROD = ("PROD", False, ["redbox.prod.uktrade.digital", "assist.businessandtrade.gov.uk", *ADDITIONAL_HOSTS])
+    DEV = ("DEV", False, [*ADDITIONAL_HOSTS])
+    UAT = ("UAT", False, [*ADDITIONAL_HOSTS])
+    LAB = ("LAB", False, [*ADDITIONAL_HOSTS])
+    PROD = ("PROD", False, [*ADDITIONAL_HOSTS])
 
 
 class Classification(StrEnum):
