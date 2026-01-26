65b791e1dbdd33550bee512e608a579a2ea56872
Doug Mills <doug.mills@digital.trade.gov.uk>
2026-01-26 15:33:42 +0000

    remove .env and code changes according to ruff checks
    Signed-off-by: DBT pre-commit check

--- data_hub_mcp/tests/test_mcp_server.py
+++ data_hub_mcp/tests/test_mcp_server.py
@@ -1,13 +1,14 @@
 import pytest
 from fastmcp.client import Client
 from fastmcp.client.transports import FastMCPTransport
-
 from mcp_server import mcp
+
 
 @pytest.fixture
 async def main_mcp_client():
     async with Client(transport=mcp) as mcp_client:
         yield mcp_client
+
 
 @pytest.mark.asyncio
 async def test_list_tools_zzzz(main_mcp_client: Client[FastMCPTransport]):
