70b2a80ac10e68d8870a7635fa5b0cfc221fba25
Doug Mills <doug.mills@digital.trade.gov.uk>
2026-03-11 07:35:18 +0000

    updates after merge
    Signed-off-by: DBT pre-commit check

--- redbox/redbox/graph/nodes/sends.py
+++ redbox/redbox/graph/nodes/sends.py
@@ -1,10 +1,8 @@
-import asyncio
-import json
-from uuid import uuid4
 import logging
 import threading
 from concurrent.futures import ThreadPoolExecutor, TimeoutError, as_completed
 from typing import Callable
+from uuid import uuid4
 
 from langchain_core.messages import AIMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
@@ -12,9 +10,10 @@ from langgraph.constants import Send
 
 from redbox.models.chain import DocumentState, RedboxState, TaskStatus
 
+import asyncio
 from mcp import ClientSession
 from mcp.client.streamable_http import streamablehttp_client
-
+import json
 
 log = logging.getLogger(__name__)
 
