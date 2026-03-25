e65247481c9d2937a345c504194bfec25b5ac4a7
David Okeke <david.okeke@madetech.com>
2026-03-25 15:45:18 +0000

    Resolved scroll issues and updated profile model field and form descriptions
    Signed-off-by: DBT pre-commit check

--- django_app/frontend/src/interaction_design_system/ids/components/stream-scroller.js
+++ django_app/frontend/src/interaction_design_system/ids/components/stream-scroller.js
@@ -5,7 +5,7 @@ import { Events, listenEvent } from "../events";
 export class StreamScroller extends HTMLElement {
     constructor() {
         super();
-        this.autoScrollEnabled = true;
+        this.autoScrollEnabled = this.scrollOnLoad;
         this.programmaticScroll = false;
         this.scrollPending = false;
         this._anchor = null;
@@ -20,21 +20,19 @@ export class StreamScroller extends HTMLElement {
     }
 
     disconnectedCallback() {
-        this.scrollContainer?.removeEventListener("scroll", () => this.#onScroll());
         this._observer?.disconnect();
     }
 
 
+    get scrollOnLoad() {
+        return (this.dataset.scrollOnLoad?.toLowerCase() == "true") ? true : false;
+    }
+
+
     #bindScrollEvents() {
-        this.scrollContainer?.addEventListener("scroll", () => this.#onScroll(), { passive: true });
         listenEvent(Events.SCROLL_TO_BOTTOM, (evt) => this.scheduleScroll(evt.detail));
     }
 
-
-    #onScroll() {
-        if (this.programmaticScroll) return;
-        this.autoScrollEnabled = this.#isAtBottom();
-    }
 
     #setupAnchor() {
         // Auto-create anchor if missing
@@ -64,20 +62,6 @@ export class StreamScroller extends HTMLElement {
     }
 
 
-    #isAtBottom(threshold = 10) {
-        if (!this.scrollContainer) return false;
-
-        let scrollElement;
-        if (this.scrollContainer instanceof Document) {
-            scrollElement = this.scrollContainer.documentElement;
-        } else {
-            scrollElement = this.scrollContainer;
-        }
-
-        return scrollElement.scrollHeight - scrollElement.scrollTop - scrollElement.clientHeight <= threshold;
-    }
-
-
     /**
      * Finds the nearest scrollable ancestor
      * @param {HTMLElement} element
@@ -95,7 +79,7 @@ export class StreamScroller extends HTMLElement {
                 overflowY === "scroll" ||
                 overflowY === "overlay";
 
-            if (canScroll && parent.scrollHeight > parent.clientHeight) {
+            if (canScroll) {
                 if (parent == document.documentElement) return document;
                 return parent;
             }
@@ -103,7 +87,7 @@ export class StreamScroller extends HTMLElement {
             parent = parent.parentElement;
         }
 
-        return document.scrollingElement || document.documentElement;
+        console.warn("scrollable element not found");
     }
 
 
@@ -138,5 +122,4 @@ export class StreamScroller extends HTMLElement {
         this.scrollPending = false;
     }
 }
-
 customElements.define("ids-stream-scroller", StreamScroller);
