08c9f4db435742de05208bc7c0ab919ced6fa124
Saisakul Chernbumroong <saisakulchernbumroong@DBT000687.local>
2026-04-09 15:09:54 +0100

    remove auto instrument datadog
    Signed-off-by: DBT pre-commit check

--- django_app/redbox_app/settings.py
+++ django_app/redbox_app/settings.py
@@ -10,6 +10,8 @@ import environ
 import sentry_sdk
 from dbt_copilot_python.database import database_from_env
 from dbt_copilot_python.error_tracking import DatadogErrorTrackingFilter
+from ddtrace import patch
+from ddtrace.llmobs import LLMObs
 from django.urls import reverse_lazy
 from django_log_formatter_asim import ASIMFormatter
 from dotenv import find_dotenv, load_dotenv
@@ -514,3 +516,8 @@ FEEDBACK_LINK = env.str(
 )
 
 PRODUCT_NAME = env.str("PRODUCT_NAME", "Redbox at DBT")
+
+# datadog
+# enable llm manual instrument
+LLMObs.enable(integrations_enabled=False, api_key=env.str("DATADOG_API_KEY", "Fake"))
+patch(langchain=True, botocore=False)
