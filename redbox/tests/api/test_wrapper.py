a73aca34b3aa7a89640283127c30446e0c2c4f26
Harry Wixley <harry.wixley@digital.trade.gov.uk>
2026-04-13 11:35:38 +0100

    nonpickablecallable tests

--- redbox/tests/api/test_wrapper.py
+++ redbox/tests/api/test_wrapper.py
@@ -1,5 +1,5 @@
 import pytest
-from redbox.api.wrapper import SensitiveValue
+from redbox.api.wrapper import SensitiveValue, NonPicklableCallable
 
 
 class TestSensitiveValue:
@@ -12,3 +12,24 @@ class TestSensitiveValue:
         sv = SensitiveValue(value)
         assert value not in repr(sv)
         assert value not in str(sv)
+
+
+class TestNonPicklableCallable:
+    @pytest.mark.parametrize(
+        "fn, args, kwargs, expected",
+        [
+            (lambda: 42, [], {}, 42),
+            (lambda x: x * 2, [5], {}, 10),
+            (lambda x, y: x + y, [3, 4], {}, 7),
+            (lambda x, y=10: x + y, [1], {"y": 20}, 21),
+        ],
+    )
+    def test_calls_wrapped_fn(self, fn, args, kwargs, expected):
+        assert NonPicklableCallable(fn)(*args, **kwargs) == expected
+
+    @pytest.mark.parametrize("fn", [lambda: "secret", lambda x: x])
+    def test_redacts_in_repr_and_str(self, fn):
+        npc = NonPicklableCallable(fn)
+        assert "secret" not in repr(npc)
+        assert "secret" not in str(npc)
+        assert repr(npc) == "NonPicklableCallable(**redacted**)"
