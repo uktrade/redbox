927cba055fb15baca55498ca697849d66e8d824e
Saisakul Chernbumroong <saisakulchernbumroong@DBT000687.local>
2026-04-28 10:38:47 +0100

    use flag list to verify too

--- django_app/redbox_app/redbox_core/services/chats.py
+++ django_app/redbox_app/redbox_core/services/chats.py
@@ -10,13 +10,7 @@ from waffle import flag_is_active
 from yarl import URL
 
 from redbox_app.redbox_core import flags
-from redbox_app.redbox_core.models import (
-    Chat,
-    ChatLLMBackend,
-    ChatMessage,
-    Tool,
-    UserTeamMembership,
-)
+from redbox_app.redbox_core.models import Chat, ChatLLMBackend, ChatMessage, Tool, UserTeamMembership
 from redbox_app.redbox_core.services import documents as documents_service
 from redbox_app.redbox_core.services import message as message_service
 from redbox_app.redbox_core.services import url as url_service
@@ -49,7 +43,7 @@ def get_context(request: HttpRequest, chat_id: UUID | None = None, slug: str | N
     if token:
         has_access = user_has_ofi_email(token) or request.user.is_superuser
 
-    if not has_access:
+    if not has_access or not flag_is_active(request, flags.ENABLE_INVEST_LENS):
         tools = tools.exclude(slug="invest-lens")
 
     messages = ChatMessage.get_messages_ordered_by_citation_priority(chat_id) if current_chat else []
