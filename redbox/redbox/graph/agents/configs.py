e6e4aa4fddbe7f1e4780c8d8e6ac2792454942a5
lizzie-perry-dbt <lizzie.perry@digital.trade.gov.uk>
2026-04-30 13:29:44 +0100

    Put parameters into  PromptVariable, where before they were in PromptConfig

--- redbox/redbox/graph/agents/configs.py
+++ redbox/redbox/graph/agents/configs.py
@@ -137,17 +137,20 @@ prompt_configs: Dict[str, PromptConfig] = {
     ),
     "Datahub_Agent": PromptConfig(
         system=prompts.DATAHUB_PROMPT + prompts.DATAHUB_QUESTION_PROMPT,
-        prompt_vars=PromptVariable(question=True),
-        chat_history=True,
-        previous_tool_error=True,
-        previous_tool_results=True,
+        prompt_vars=PromptVariable(
+            question=True, chat_history=True, previous_tool_error=True, previous_tool_results=True
+        ),
     ),
     "Knowledge_Base_Retrieval_Agent": PromptConfig(
         system=prompts.INTERNAL_RETRIEVAL_AGENT_PROMPT
         + prompts.KNOWLEDGE_BASE_METADTA
         + prompts.PREVIOUS_AGENT_RESULTS,
-        prompt_vars=PromptVariable(task=True, expected_output=True, knowledge_base_metadata=True),
-        previous_agents_results=True,
+        prompt_vars=PromptVariable(
+            task=True,
+            expected_output=True,
+            knowledge_base_metadata=True,
+            previous_agents_results=True,
+        ),
     ),
     "Artifact_Builder_Agent": PromptConfig(
         system=prompts.ARTIFACT_BUILDER_AGENT_PROMPT + prompts.KNOWLEDGE_BASE_METADTA,
