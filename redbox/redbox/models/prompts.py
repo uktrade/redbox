e2873ef3fdb0dc3aca48b60efc82ab3a7db16c88
lizzie-perry-dbt <lizzie.perry@digital.trade.gov.uk>
2026-05-27 15:03:12 +0100

    Changed so the tools return a non-empty message instead of an empty list. And updated teh planner prompt to avoid mis-routing when documents have not been selected

--- redbox/redbox/models/prompts.py
+++ redbox/redbox/models/prompts.py
@@ -454,6 +454,8 @@ PLANNER_PROMPT_BOTTOM = """
 2. Only use External_Retrieval_Agent if the query specifically requests external data sources.
 3. Only use Artifact_Builder_Agent if there is specific artifact file that match user request.
 4. The Evaluator_Agent must be used for exactly once in the plan, and it must be the final task. Never insert Evaluator_Agent as an intermediate step. The Evaluator_Agent at the end ensures consistency and quality in the final output delivered to the user.
+5. If the user has not selected any documents; do NOT route to Internal_Retrieval_Agent, Tabular_Agent or Summarisation_Agent. Instead ask Evaluator_Agent to inform the user that they have not selected any documents, and will need to if they want to use these agents.
+6. If the knowledge base is emptry; do NOT route to Knowledge_Base_Agent or Artifact_builder_Agent.
 
 If a user asks to summarise a document, ALWAYS call Summarisation_Agent and do not call other agents.
 
