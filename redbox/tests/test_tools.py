63ca124febce6153728a5839334db08bee4b7173
lizzie-perry-dbt <lizzie.perry@digital.trade.gov.uk>
2026-06-04 15:56:30 +0100

    revert to oringinal tests

--- redbox/tests/test_tools.py
+++ redbox/tests/test_tools.py
@@ -509,43 +509,20 @@ def test_search_documents_tool_merge_produces_empty_returns_text(mocker: MockerF
     assert message.artifact == []
 
 
-def test_search_knowledge_base_tool_merge_produces_empty_returns_text(
-    mocker: MockerFixture,
-    env: Settings,
-):
+def test_search_knowledge_base_tool_merge_produces_empty_returns_text(mocker: MockerFixture, env: Settings):
     """
-    If merge_documents returns empty or whitespace only, despite documents
-    existing, the tool must return a non-empty string, for knowledge base.
+    If  merge_documents returns empty or whitespace only, despite documents existing, the tool must return a non-empty string, for knowledge base
     """
     mock_es_client = mocker.MagicMock(spec=OpenSearch)
     mock_es_client.search.return_value = {
-        "hits": {
-            "hits": [
-                {
-                    "_index": "index",
-                    "_id": "1",
-                    "_score": 1.0,
-                    "_source": {
-                        "text": "hello world",
-                        "metadata": {
-                            "uri": "abc",
-                        },
-                    },
-                }
-            ]
-        }
+        "hits": {"hits": [{"_id": "1", "_score": 1.0, "_source": {"text": "hello world", "metadata": {"uri": "abc"}}}]}
     }
-
-    mocker.patch(
-        "redbox.graph.nodes.tools.merge_documents",
-        return_value=[],
-    )
-
+    mocker.patch("redbox.graph.nodes.tools.merge_documents", return_value=[])
     embedding_model = FakeEmbeddings(size=1024)
 
     knowledge_base_search = build_search_documents_tool(
         es_client=mock_es_client,
-        index_name="index",
+        index_name="_index",
         embedding_model=embedding_model,
         embedding_field_name=env.embedding_document_field_name,
         chunk_resolution=ChunkResolution.normal,
@@ -553,7 +530,6 @@ def test_search_knowledge_base_tool_merge_produces_empty_returns_text(
     )
 
     tool_node = ToolNode(tools=[knowledge_base_search])
-
     result_state = tool_node.invoke(
         _make_search_tool_state(
             "example prompt",
