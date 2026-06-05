6f80ea289d803079aff31ff21ae8fe9dc715a517
Harry Wixley <harry.wixley@digital.trade.gov.uk>
2026-06-05 17:22:25 +0100

    add logs

--- redbox/redbox/graph/nodes/cache/tools.py
+++ redbox/redbox/graph/nodes/cache/tools.py
@@ -1,3 +1,4 @@
+import logging
 import asyncio
 import time
 from typing import Callable, Any
@@ -5,6 +6,8 @@ from pydantic import BaseModel, Field
 
 from redbox.api.wrapper import SensitiveValue
 from redbox.graph.nodes.tools import get_datahub_mcp_tools
+
+log = logging.getLogger(__name__)
 
 
 class CacheEntry(BaseModel):
@@ -26,10 +29,14 @@ async def get_cached_datahub_mcp_tools(sso_token_getter: Callable[[], str]) -> l
     token_key = SensitiveValue(value=raw_token)
 
     if (entry := _datahub_mcp_tool_cache.get(token_key)) and not entry.is_expired():
+        log.warning("[get_cached_datahub_mcp_tools] Fetching Datahub_Agent MCP tools from cache...")
         return entry.value
 
     async with _datahub_mcp_tool_cache_lock:
         if not (entry := _datahub_mcp_tool_cache.get(token_key)) or entry.is_expired():
+            log.warning(
+                "[get_cached_datahub_mcp_tools] Fetching Datahub_Agent MCP tools from server and updating cache..."
+            )
             _datahub_mcp_tool_cache[token_key] = CacheEntry(
                 value=await get_datahub_mcp_tools(sso_token_getter=sso_token_getter)
             )
