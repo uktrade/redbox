0703b6920605c87803d3b05bb5d24908d90c030a
Chris Hopkins <chris.hopkins@madetech.com>
2026-07-29 14:58:06 +0100

    wip: testing e2e

---
+++ django_app/tests/playwright/test_e2e.py
@@ -0,0 +1,45 @@
+import logging
+import os
+
+from pages import LandingPage
+from playwright.sync_api import Page
+
+logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO"))
+logger = logging.getLogger(__name__)
+
+
+BASE_URL = "https://dev.assist.uktrade.digital"
+
+
+def test_user_journey(page: Page):
+    """End to end user journey test.
+
+    Simulates a single user journey through the application, running against the full suite of microservices.
+
+    Uses the Page Object Model - see https://pinboard.in/u:brunns/t:page-object for some resources explaining this.
+    Please add to the page objects in `pages.py` where necessary - don't put page specific logic at this level.
+
+    We should not be asserting anything about AI generated content in this test, aside from asserting that there
+    is some."""
+
+    # create_user(email_address)
+
+    # Landing page
+    landing_page = LandingPage(page, BASE_URL)
+
+    # Sign in
+    chats_page = landing_page.sign_in()
+
+
+def test_support_pages(page: Page):
+    # Landing page
+    landing_page = LandingPage(page, BASE_URL)
+
+    # Privacy page
+    landing_page.navigate_to_privacy_page()
+
+    # Accessibility page
+    landing_page.navigate_to_accessibility_page()
+
+    # Support page
+    landing_page.navigate_to_support_page()
