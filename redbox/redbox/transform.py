e2f8b343dc8ef2ef581bc32f1dbb7e22385f34ae
koakande <46060542+kbakande@users.noreply.github.com>
2026-09-21 19:03:29 +0100

    assist-1679-reverse to the old logic
    Signed-off-by: DBT pre-commit check

--- redbox/redbox/transform.py
+++ redbox/redbox/transform.py
@@ -294,7 +294,7 @@ def merge_documents(initial: list[Document], adjacent: list[Document]) -> list[D
     Privileges the initial score.
     """
     # Subtitute boosted scores for the semantically retrieved chunks
-    merged_dict = to_document_mapping(initial) | to_document_mapping(adjacent)
+    merged_dict = to_document_mapping(adjacent) | to_document_mapping(initial)
 
     return sorted(list(merged_dict.values()), key=lambda d: -d.metadata["score"])[: len(initial)]
 
