d85c0f00c5c3eb69f5b469b178afbd896e89a913
koakande <46060542+kbakande@users.noreply.github.com>
2026-09-22 08:25:41 +0000

    assist-1679-mad enumbers up to 2decimal
    Signed-off-by: DBT pre-commit check

--- redbox/tests/test_transform.py
+++ redbox/tests/test_transform.py
@@ -320,11 +320,11 @@ def test_merge_documents():
     # Adjacent neighbours displace initial semantically relevant chunks
     # [0.7A, 0.68B, 0.60C] + [1.40A, 1.36B, 1.2C, 1.16A1, 0.96A2] => [1.16A1, 0.96A2, 0.7A]
     initial = [copy.deepcopy(d) for d in docs_1]
-    for doc, score in zip(initial, [0.7, 0.68, 0.60]):
+    for doc, score in zip(initial, [0.70, 0.68, 0.60]):
         doc.metadata["score"] = score
 
     boosted_initial = [copy.deepcopy(d) for d in docs_1]
-    for doc, score in zip(boosted_initial, [1.4, 1.36, 1.2]):
+    for doc, score in zip(boosted_initial, [1.40, 1.36, 1.20]):
         doc.metadata["score"] = score
 
     # Set up neighbours
@@ -340,7 +340,8 @@ def test_merge_documents():
 
     merged_3 = merge_documents(initial=initial, adjacent=adjacent)
 
-    assert [d.metadata["score"] for d in merged_3] == [1.16, 0.96, 0.7], (
+    # Adjacent neighbours displace initial semantically relevant chunks
+    assert [d.metadata["score"] for d in merged_3] == [1.16, 0.96, 0.70], (
         "Strong neighbours should displace weak initials"
     )
 
